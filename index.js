bf9086087ccca7d2721283acf38eabd642f49daa
IrinaRudakova <51534532+IrinaRudakova@users.noreply.github.com>
2019-06-20 20:55:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,20 +7,18 @@ function moreClick(event, cardNamber) {
     cardNamber = cardNamber || "";
     var mobileMenu = document.querySelector(".item-window" + cardNamber);
     var display = mobileMenu.style.display;
-    if (display =="" || display=="none") {
-        $(".item-window" + cardNamber).fadeIn(7000);
-        mobileMenu.style.display = "flex"
-    } else {
-        $(".item-window" + cardNamber).fadeOut(2000);
-    }
+    $(".item-window" + cardNamber).fadeIn(1000);
+    mobileMenu.style.display = "flex"
 }
 
 var hamburgerIcon = document.getElementsByClassName("menu-icon")[0];
 hamburgerIcon.addEventListener("click", hamburgerClick);
 var a = document.getElementsByClassName("card-button")[1];
 a.addEventListener("click", moreClick);
-var b = document.getElementsByClassName("item-close")[0];
-b.addEventListener("click", moreClick);
+
+$(".item-close").click(function () {
+    $(".item-window").fadeOut(1000);
+});
 
 $(".card-button.one").click(
     function (event) {
@@ -36,6 +34,6 @@ $(".card-button.two").click(
 
 $("a[href='#']").click(function () {
     $("html, body").animate({
-        scrollTop: $("#mail").offset().top
+        scrollTop: $("#main").offset().top
     }, 1000);
 });
