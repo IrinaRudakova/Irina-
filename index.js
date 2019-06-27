ae230c953fe53a9a4d1f73e40cae85994c841845
IrinaRudakova <51534532+IrinaRudakova@users.noreply.github.com>
2019-06-27 20:27:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ $(".card-button.two").click(
     }
 )
 
+$(".card-button.three").click(
+    function (event) {
+        moreClick(event, ".one");
+    }
+)
+
+$(".card-button.four").click(
+    function (event) {
+        moreClick(event, ".one");
+    }
+)
+
 $("a[href='#']").click(function () {
     $("html, body").animate({
         scrollTop: $("#main").offset().top
