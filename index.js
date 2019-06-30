5846b6ab5eaf836e7ec9c715e67ee4d68145ce3e
IrinaRudakova <51534532+IrinaRudakova@users.noreply.github.com>
2019-06-30 16:11:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-function hamburgerClick() {
-   $(".mobile-menu").slideToggle(600);
-}
-
-function moreClick(event, cardNamber) {
-    event.preventDefault();
-    cardNamber = cardNamber || "";
-    var mobileMenu = document.querySelector(".item-window" + cardNamber);
-    var display = mobileMenu.style.display;
-    $(".item-window" + cardNamber).fadeIn(1000);
-    mobileMenu.style.display = "flex"
-}
-
-var hamburgerIcon = document.getElementsByClassName("menu-icon")[0];
-hamburgerIcon.addEventListener("click", hamburgerClick);
-var a = document.getElementsByClassName("card-button")[1];
-a.addEventListener("click", moreClick);
-
-$(".item-close").click(function () {
-    $(".item-window").fadeOut(1000);
-});
-
-$(".card-button.one").click(
-    function (event) {
-        moreClick(event, ".one");
-    }
-)
-
-$(".card-button.two").click(
-    function (event) {
-        moreClick(event, ".two");
-    }
-)
-
 $(".card-button.three").click(
     function (event) {
         moreClick(event, ".one");
@@ -46,6 +12,4 @@ $(".card-button.four").click(
 
 $("a[href='#']").click(function () {
     $("html, body").animate({
-        scrollTop: $("#main").offset().top
-    }, 1000);
-});
+        scrollTop: $("#main").offset().top})})
